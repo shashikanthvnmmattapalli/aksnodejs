a6cd24aefb72331818d8d3015ae1c7992cb8b15f
shashikanthvnmmattapalli <45655351+shashikanthvnmmattapalli@users.noreply.github.com>
2023-01-22 22:24:11 +0530

    Update vmcreation.ps1

--- vmcreation.ps1
+++ vmcreation.ps1
@@ -2,10 +2,10 @@
 
 #Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
 Install-Module Az.Accounts -Force -AllowClobber
-$ApplicationId= "f11d280f-1fa9-4ea2-af02-e54d0911e5aa"
-$ClientSecret= "2Kh8Q~LgOdDqSHcDFdntBfd.LBa2cVAl0h7p7aYX"
-$TenantId = "dd2b7915-1762-4379-ac95-eb8d51d8dbe2"
-$subscriptionID ="4d7e6e1d-80af-4eab-a867-982ace56a3a7"
+$ApplicationId= ""
+$ClientSecret= ""
+$TenantId = ""
+$subscriptionID =""
 $userPassword = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId , $userPassword
 Connect-AzAccount -ServicePrincipal -TenantId $TenantId -SubscriptionId $subscriptionID -Credential $Credential
